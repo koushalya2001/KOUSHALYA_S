3f491893ad74a5150decda4be81f886f644350a3
skoushi <61419264+skoushi@users.noreply.github.com>
2020-02-24 19:28:09 +0530

    Create leaderfinal.js

---
+++ leaderfinal.js
@@ -0,0 +1,29 @@
+var express=require('express');
+var app=express();
+app.listen(3000);
+app.set('view engine',express);
+var mongoose = require("mongoose");
+mongoose.connect('mongodb://localhost:27017/game', {useNewUrlParser: true,useUnifiedTopology: true});
+var profileSchema=new mongoose.Schema({
+  name:String,
+  level:Number,
+  team:String,
+//  created_at:{type:Date,default:Date.now},
+  updated_at:{type:Date,default:Date.now}
+});var arr=[];
+var ProfileSchema= mongoose.model('profileSchema',profileSchema);
+app.get('/show',function(req,res){
+ProfileSchema.find({},function(err, docs) {
+if (err)
+console.log(err);
+console.log(docs);
+}).sort([["level", "descending"], ["updated_at", "ascending"]]);
+res.render('i',{gamer:allusers});
+
+/*ProfileSchema.estimatedDocumentCount({},function(err,count){if(err) //TO REDUCE DATABASE ACCESS TIME
+console.log(err);
+res.render('i',{c:count});
+});
+mongoose.connection.close();*/ 
+});
+//var om=require('./gamein');
